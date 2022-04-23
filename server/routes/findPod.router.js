7eb3385f29386248469ac524466f7987dfd18574
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-23 15:27:07 -0500

    Create findPod router

---
+++ server/routes/findPod.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+    rejectUnauthenticated,
+  } = require('../modules/authentication-middleware');
+
+router.get('/:key_code', rejectUnauthenticated, (req, res) => {
+    
+    
+    const query = `SELECT * FROM pods WHERE "key_code" = $1`;
+    const values = [req.params.key_code]
+    pool.query(query, values)
+      .then( result => {
+        res.send(result.rows);
+        console.log(result);
+        
+      })
+      .catch(err => {
+        console.log('ERROR FINDING POD', err);
+        res.sendStatus(500)
+      })
+  
+  });
+
+  module.exports = router;
