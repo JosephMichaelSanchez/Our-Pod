6d7f33c30e1e7ca91cf524d9080775bde8838b3a
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-23 14:27:24 -0500

    Create createPod router

---
+++ server/routes/createPod.router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+    rejectUnauthenticated,
+  } = require('../modules/authentication-middleware');
+
+  router.post('/', rejectUnauthenticated, (req, res) => {
+    const id = req.user.id;
+
+    const query = `INSERT INTO pods ("pod_name", "key_code", "admin_id")
+    VALUES ($1, $2, $3);`;
+    const values = [req.body.pod_name, req.body.key_code, id]
+    pool.query(query, values)
+        .then(result => {
+            res.sendStatus(201)
+        })
+        .catch(err => {
+            console.log('ERROR ADDING HOUSE DETAILS', err);
+            res.sendStatus(500)
+        })
+
+});
+
+module.exports = router;
