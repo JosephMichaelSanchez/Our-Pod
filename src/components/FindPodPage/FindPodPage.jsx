2aaa3c887b640698402bc05f4f0f93041f9bd823
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-23 15:06:47 -0500

    Add input handleSubmit and dispatch to FindPodPage

--- src/components/FindPodPage/FindPodPage.jsx
+++ src/components/FindPodPage/FindPodPage.jsx
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './FindPodPage.css'
 
 function FindPodPage() {
 
+    const [keyCode, setKeyCode] = useState('');
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(keyCode);
+
+        dispatch({
+            type: 'FIND_POD',
+            payload: keyCode
+        })
+
+        // history.push('/joinpod');
+    }
+
     return (
         <>
             <div className="body">
-                <h3>THIS IS THE FIND POD PAGE</h3>
+                <h2>THIS IS THE FIND POD PAGE</h2>
+                <h3>ENTER THE KEY CODE OF THE POD YOU WOULD LIKE TO JOIN</h3>
+                <input type="text" name="keyCode" placeholder='KEY CODE' onChange={(event) => setKeyCode(event.target.value)} /> <button onClick={handleSubmit}>FIND POD</button>
+
             </div>
         </>
     )
