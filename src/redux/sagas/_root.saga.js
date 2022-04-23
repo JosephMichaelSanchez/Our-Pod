83e9ab872185f7f9258c2af4caa7b4963d82c4d5
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-23 16:56:43 -0500

    Create joinPodSaga and import in rootSaga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -5,6 +5,7 @@ import userSaga from './user.saga';
 import houseDetailsSaga from './houseDetails.saga';
 import createPodSaga from './createPod.saga';
 import findPodSaga from './findPod.saga';
+import joinPodSaga from './joinPod.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -21,5 +22,6 @@ export default function* rootSaga() {
     houseDetailsSaga(),
     createPodSaga(),
     findPodSaga(),
+    joinPodSaga(),
   ]);
 }
---
+++ src/redux/sagas/joinPod.saga.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* joinPodSaga () {
+    yield takeLatest('JOIN_POD', joinPod);
+}
+
+function* joinPod(action){
+    console.log('IN JOIN POD');
+    const id = action.payload;
+    
+    try {
+    yield axios.put(`/api/joinpod/${id}`)
+    } catch {
+        console.log('ERROR IN joinPod');
+        }
+}
+
+export default joinPodSaga;
