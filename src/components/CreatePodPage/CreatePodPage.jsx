65b53b4ed654dc6123f9e2e93207a41148f9eb40
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-23 11:29:37 -0500

    Add form to  CreatePodPage

--- src/components/CreatePodPage/CreatePodPage.jsx
+++ src/components/CreatePodPage/CreatePodPage.jsx
@@ -4,10 +4,47 @@ import './CreatePodPage.css'
 
 function CreatePodPage() {
 
+    const dispatch = useDispatch();
+    const [podName, setPodName] = useState('');
+    const [keyCode, seyKeyCode] = useState('');
+
+    const handleSubmit = () => {
+        console.log('clicked');
+    }
+
     return (
         <>
             <div className="body">
                 <h2>THIS IS THE CREATE A POD PAGE</h2>
+
+                <form>
+                    <div>
+
+                        Pod Name:
+                        <input
+                            type="text"
+                            name="podName"
+                            value={podName}
+                            placeholder='POD NAME'
+                            onChange={(event) => setPodName(event.target.value)}
+                        />
+
+                    </div>
+                    <div>
+
+                        Pod Key Code:
+                        <input
+                            type="text"
+                            name="keyCode"
+                            value={keyCode}
+                            placeholder='KEY CODE'
+                            onChange={(event) => setKeyCode(event.target.value)}
+                        />
+
+                    </div>
+
+                    <button onClick={handleSubmit}>CREATE POD</button>
+                </form>
             </div>
         </>
     )
