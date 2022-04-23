57203cf0c768c00c308edc13502ac82610fba028
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-23 16:08:51 -0500

    Create JoinPodPage component and import css

---
+++ src/components/JoinPodPage/JoinPodPage.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './JoinPodPage.css'
+
+function JoinPodPage() {
+
+    return (
+        <>
+            <div className="body">
+                <h2>THIS IS THE JOIN POD PAGE</h2>
+            </div>
+        </>
+    )
+}
+
+
+export default JoinPodPage;
