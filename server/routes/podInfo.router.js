e6db0c0e2eb9614866eceacd00a37cdbdb1e553a
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-24 18:37:20 -0500

    Create podInfo router

---
+++ server/routes/podInfo.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+    rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    const id = req.user.pod_id
+
+    const query = `SELECT * FROM pods WHERE "id" = $1`;
+    const values = [id]
+    pool.query(query, values)
+        .then(result => {
+            res.send(result.rows);
+            console.log(result);
+
+        })
+        .catch(err => {
+            console.log('ERROR GETTING POD INFO', err);
+            res.sendStatus(500)
+        })
+
+});
+
+module.exports = router;
