8ff14429d355fb92ff30f7828d3fe58667478465
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-24 16:09:25 -0500

    Create PodMember component for mapping podList, and import it into MyPodPage

--- src/components/MyPodPage/MyPodPage.jsx
+++ src/components/MyPodPage/MyPodPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+import PodMember from '../PodMember/PodMember';
 
 import './MyPodPage.css'
 
---
+++ src/components/PodMember/PodMember.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function PodMember({member}) {
+
+    return (
+        <>
+            <div key={member.id} className="memberDiv">
+                <p>{member.first_name}</p>
+                <p>{member.last_name}</p>
+                <p>{member.address}</p>
+                <p>{member.phone}</p>
+                <p>{member.email}</p>
+
+            </div>
+        </>
+
+    )
+}
+
+export default PodMember;
