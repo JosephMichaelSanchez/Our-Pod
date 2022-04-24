a722445ba0771e86be62db3f9aa2b179ff25d47a
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-24 09:22:21 -0500

    Add My Pod to Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -28,6 +28,10 @@ function Nav() {
               Home
             </Link>
 
+            <Link className="navLink" to="/mypod">
+              My Pod
+            </Link>
+
             <Link className="navLink" to="/myhousedetails">
               My House Details
             </Link>
