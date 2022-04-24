a1ec6523549a94b59803f0306941cd3f7c09af87
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-24 12:16:29 -0500

    Add useParams to MyPodPage

--- src/components/MyPodPage/MyPodPage.jsx
+++ src/components/MyPodPage/MyPodPage.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import './MyPodPage.css'
 
 function MyPodPage() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const {pod_id} = useParams();
+    const user = useSelector(store => store.user);
+
+    
+
 
     return (
         <>
