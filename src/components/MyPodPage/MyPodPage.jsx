96197d20ddaf1d41a07ebb286c8a157a28804c8c
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-24 11:35:03 -0500

    Create MyPodPage component

---
+++ src/components/MyPodPage/MyPodPage.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import './MyPodPage.css'
+
+function MyPodPage() {
+
+    return (
+        <>
+            <div className="body">
+                <h2>MY POD PAGE</h2>
+            </div>
+        </>
+    )
+}
+
+
+export default MyPodPage;
