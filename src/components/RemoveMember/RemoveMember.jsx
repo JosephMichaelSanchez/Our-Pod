ae1d7348e5c5c429ab4f48d2e45d7cde3067d7fe
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-25 09:28:50 -0500

    Create RemoveMember component

---
+++ src/components/RemoveMember/RemoveMember.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+
+function RemoveMember() {
+
+    return (
+        <>
+            <div className="body">
+                <h2>THIS IS THE REMOVE MEMBER PAGE</h2>
+            </div>
+        </>
+    )
+
+}
+
+export default RemoveMember;
