6e2baad8eab47e9cdcfd426cfc4c1328f07a224d
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-25 10:47:30 -0500

    Add sweet alert to confirm pod memer removal

--- src/components/KickMember/KickMember.jsx
+++ src/components/KickMember/KickMember.jsx
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './KickMember.css'
+import './KickMember.css';
+import swal from 'sweetalert';
 
 function KickMember({member}) {
 
     const dispatch = useDispatch();
     const user = useSelector(store => store.user);
     const podInfo = useSelector(store => store.podInfoReducer);
+
+
+    const handleRemove = () => {
+        console.log('clicked');
+        swal({
+            title: "Are you sure?",
+            text: `Are you sure you want to remove ${member.first_name} ${member.last_name} from your pod?`,
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal(`${member.first_name} ${member.last_name} has been removed from your pod.`, {
+                icon: "success",
+              });
+              console.log('REMOVE');
+            } else {
+              swal(`${member.first_name} ${member.last_name} will not be deleted.`);
+            }
+          });
+    }
 
     return (
         <>
@@ -16,7 +39,7 @@ function KickMember({member}) {
                 <p>{member.address}</p>
                 <p>{member.phone}</p>
                 <p>{member.email}</p>
-                <p>{member.last_name != user.last_name ? <button>REMOVE</button> : <span>ADMIN</span>}</p>
+                <p>{member.last_name != user.last_name ? <button onClick={handleRemove}>REMOVE</button> : <span>ADMIN</span>}</p>
 
             </div>
         </>
