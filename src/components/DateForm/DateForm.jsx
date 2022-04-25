a98fa64a8b8d11bb6395d3d361cae97ec967a279
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-24 20:15:06 -0500

    Create DateForm component

---
+++ src/components/DateForm/DateForm.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function DateForm() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const user = useSelector(store => store.user);
+    const podInfo = useSelector(store => store.podInfoReducer);
+    const [newDate, setNewDate] = useState('');
+
+    // useEffect(() => {
+    //     dispatch({
+    //         type: 'GET_USER_POD',
+    //         payload: user.pod_id
+    //     })
+    //     dispatch({
+    //         type: 'GET_POD_INFO',
+    //         payload: user.pod_id
+    //     })
+    // }, [])
+
+    const handleSubmit = () => {
+
+        if (newDate.length === 10) {
+            console.log(newDate);
+
+            const Id = podInfo.id
+
+            // const newHostingDate = {
+            //     date: newDate,
+            //     podId: Id
+            // }
+
+            // console.log(newHostingDate);
+
+            dispatch({
+                type: 'ADD_NEW_DATE',
+                payload: newDate
+            })
+
+        } else {
+            console.log('not long enough');
+        }
+
+        setNewDate('');
+
+
+
+    }
+
+
+
+
+
+    return (
+        <>
+            <div className="body">
+                <h3>THIS IS THE DATE FORM</h3>
+                <form>
+                    <div>
+                        <label htmlFor="newdate">
+                            Add New Date:
+                            <input
+                                type="text"
+                                name="newdate"
+                                value={newDate}
+                                placeholder='YYYY/MM/DD'
+                                onChange={(event) => setNewDate(event.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <button onClick={handleSubmit}>ADD DATE</button>
+                        <div>
+                            <button onClick={() => { history.push(`/mypod`) }}>BACK TO POD PAGE</button>
+                        </div>
+                    </div>
+
+
+                </form>
+            </div>
+        </>
+    )
+}
+
+
+
+export default DateForm;
