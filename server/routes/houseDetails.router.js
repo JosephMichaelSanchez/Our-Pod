f6ac33c01c3e5bf88c92ccf067a712b7fdad5194
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-27 10:23:49 -0500

    Add conditional in houseDetails router to change null values to req.user

--- server/routes/houseDetails.router.js
+++ server/routes/houseDetails.router.js
@@ -8,9 +8,45 @@ const {
   router.post('/', rejectUnauthenticated, (req, res) => {
     const id = req.user.id;
 
+    let firstName = null;
+    let lastName = null;
+    let address = null;
+    let phone = null;
+    let email = null;
+
+    if (req.body.first_name.length < 1) {
+        firstName = req.user.first_name
+    } else {
+        firstName = req.body.first_name
+    }
+
+    if (req.body.last_name.length < 1) {
+        lastName = req.user.last_name
+    } else {
+        lastName = req.body.last_name
+    }
+
+    if (req.body.address.length < 1) {
+        address = req.user.address
+    } else {
+        address = req.body.address
+    }
+    if (req.body.phone.length < 1) {
+        phone = req.user.phone
+    } else {
+        phone = req.body.phone
+    }
+
+    if (req.body.email.length < 1) {
+        email = req.user.email
+    } else {
+        lastName = req.body.email
+    }
+
+
     const query = `UPDATE "user" SET "first_name" = $1, "last_name" = $2, "address" = $3, "phone" = $4, "email" = $5
     WHERE "id" = $6;`;
-    const values = [req.body.first_name, req.body.last_name, req.body.address, req.body.phone, req.body.email, id]
+    const values = [firstName, lastName, address, phone, email, id]
     pool.query(query, values)
         .then(result => {
             res.sendStatus(201)
