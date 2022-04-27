a12843628f6c7ca1d3a4dfbcc5a630e6e7f31581
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-27 15:31:32 -0500

    Change copyright to my name, change welcome greeting

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -7,7 +7,7 @@ function UserPage() {
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
+      <h2>Welcome to OurPod, {user.username}!</h2>
       <LogOutButton className="btn btn-dark" />
     </div>
   );
