9b342478f4e84f719fbb1a3f4876f52c2e1e55ba
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-28 07:52:18 -0500

    Add useEffect to userpage to attempt to get pod info

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+        type: 'GET_USER_POD'
+    })
+    dispatch({
+        type: 'GET_POD_INFO'
+    })
+    dispatch({
+        type: 'GET_DATES'
+    })
+}, [])
+
+
+
   return (
     <div className="container">
       <h2>Welcome to Our Pod, {user.username}!</h2>
