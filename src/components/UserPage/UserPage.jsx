d3db29d634897cb19821db1ea126dded90ef0daf
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-29 14:14:12 -0500

    Add list to UserPage

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector, useDispatch} from 'react-redux';
+import './UserPage.css'
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -25,6 +26,15 @@ function UserPage() {
     <div className="container">
       <h2>Welcome to Our Pod, {user.username}!</h2>
       <LogOutButton className="btn btn-dark" />
+      <div className="directionsDiv">
+        <h4>Use the Navigation Bar to move around the site!</h4>
+        <ul>
+          <li>"My Pod"  will take you to your pod</li>
+          <li>"My House Details" is where you provide your contact information</li>
+          <li>"Create a Pod" is where you can make your very own pod!</li>
+          <li>"Join a Pod" is where you can join an existing pod</li>
+        </ul>
+      </div>
     </div>
   );
 }
