ff1c52a577706bcb506d7f0d9bfc3e714e60b1dd
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-05-01 11:20:10 -0500

    Add instructions to the Info Page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './InfoPage.css'
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -8,7 +9,17 @@ import React from 'react';
 function InfoPage() {
   return (
     <div className="container">
-      <p>Info Page</p>
+      <div className="instructionsDiv">
+        <h3>How to use Our Pod</h3>
+        <ul>
+          <p>Don't have a Pod to join? Create one on the "Create a Pod" page.</p>
+          <p>Have a Key Code for an existing pod? Enter your code on the "Join a Pod" page.</p>
+          <p>After joining your pod, enter your contact information on the "My House Details" page.</p>
+          <p>Now you're ready to head to the "My Pod" page to see your podmates' info and schedule dates!</p>
+          <p>If you are an Admin, use the "Add Dates" button on the "My Pod" page to edit the list of dates.</p>
+          <p>Admins can also use the "Edit Pod" button to remove members of their pod.</p>
+        </ul>
+      </div>
     </div>
   );
 }
