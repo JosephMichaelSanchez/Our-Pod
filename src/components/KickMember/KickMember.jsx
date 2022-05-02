578ee72fca1029edcc4bc80c579111dcb085e127
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-05-02 14:32:48 -0500

    Add conditionals on Admin pages

--- src/components/KickMember/KickMember.jsx
+++ src/components/KickMember/KickMember.jsx
@@ -3,56 +3,57 @@ import { useDispatch, useSelector } from 'react-redux';
 import './KickMember.css';
 import swal from 'sweetalert';
 
-function KickMember({member}) {
+function KickMember({ member }) {
 
-    const dispatch = useDispatch();
-    const user = useSelector(store => store.user);
-    const podInfo = useSelector(store => store.podInfoReducer);
+  const dispatch = useDispatch();
+  const user = useSelector(store => store.user);
+  const podInfo = useSelector(store => store.podInfoReducer);
 
 
-    const handleRemove = () => {
+  const handleRemove = () => {
 
-        const kick = {
-            first_name: member.first_name,
-            email: member.email
-        }
-        swal({
-            title: "Are you sure?",
-            text: `Are you sure you want to remove ${member.first_name} ${member.last_name} from your pod?`,
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-              swal(`${member.first_name} ${member.last_name} has been removed from your pod.`, {
-                icon: "success",
-              });
-              console.log('REMOVE');
-              dispatch({
-                  type: 'KICK_MEMBER',
-                  payload: kick
-              })
-            } else {
-              swal(`${member.first_name} ${member.last_name} will not be deleted.`);
-            }
-          });
+    const kick = {
+      first_name: member.first_name,
+      email: member.email
     }
+    swal({
+      title: "Are you sure?",
+      text: `Are you sure you want to remove ${member.first_name} ${member.last_name} from your pod?`,
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal(`${member.first_name} ${member.last_name} has been removed from your pod.`, {
+            icon: "success",
+          });
+          console.log('REMOVE');
+          dispatch({
+            type: 'KICK_MEMBER',
+            payload: kick
+          })
+        } else {
+          swal(`${member.first_name} ${member.last_name} will not be deleted.`);
+        }
+      });
+  }
 
-    return (
-        <>
-            <div key={member.id} className="memberDiv">
-                <p>{member.first_name}</p>
-                <p>{member.last_name}</p>
-                <p>{member.address}</p>
-                <p>{member.phone}</p>
-                <p>{member.email}</p>
-                <p>{member.last_name != user.last_name ? <button className="btn btn-light" onClick={handleRemove}>REMOVE</button> : <span>ADMIN</span>}</p>
+  return (
+    <>
+      {user.id === podInfo.admin_id ?
+        <div key={member.id} className="memberDiv">
+          <p>{member.first_name}</p>
+          <p>{member.last_name}</p>
+          <p>{member.address}</p>
+          <p>{member.phone}</p>
+          <p>{member.email}</p>
+          <p>{member.last_name != user.last_name ? <button className="btn btn-light" onClick={handleRemove}>REMOVE</button> : <span>ADMIN</span>}</p>
 
-            </div>
-        </>
+        </div> : <p>You don't belong here!</p>}
+    </>
 
-    )
+  )
 }
 
 export default KickMember;
